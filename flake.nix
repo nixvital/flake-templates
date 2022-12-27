a21f6c3677afdff63abd54dcd53850f2d021310a
Break Yang <breakds@gmail.com>
2022-12-27 12:15:13 -0800

    Add the initial template called torch-basics

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "A collection of flake templates as starting points for your awesome projects";
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    templates.torch-basics = {
+      path = ./templates/torch-basics;
+      description = "Template for a basic machine learning project with PyTorch";
+    };
+  };
+}
