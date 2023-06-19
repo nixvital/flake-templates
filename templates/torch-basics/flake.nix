22a51a29e912f00c1ad267ee3619073e7cfd6d9a
Break Yang <breakds@gmail.com>
2023-06-18 17:21:17 -0700

    Bump up torch basics to 23.05

--- templates/torch-basics/flake.nix
+++ templates/torch-basics/flake.nix
@@ -2,7 +2,7 @@
   description = "An awesome machine-learning project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     utils.url = "github:numtide/flake-utils";
 
